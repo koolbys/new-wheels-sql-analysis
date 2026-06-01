9c874edb77650413a1bde52a79323c35962c5da7
koolbys <briana.y.salinas@gmail.com>
2026-06-01 15:49:48 -0500

    Create sql_queries.sql

---
+++ sql_queries.sql
@@ -0,0 +1,11 @@
+/*
+New Wheels Business Performance Analysis
+Author: Briana Salinas
+
+This file contains SQL queries used to analyze:
+- Customer distribution
+- Customer satisfaction
+- Manufacturer preferences
+- Revenue trends
+- Shipping performance
+*/
